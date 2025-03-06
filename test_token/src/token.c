4cfcc70b7331b2c9247e8aae2968126106efd418
Lise <165786617+Noobliz@users.noreply.github.com>
2025-03-06 15:58:58 +0100

    adding split_multi
    I thought I should pimp the split so we can add various separators. Looks like the tokens are correct with this. You can make more tests and change what you want :)

--- test_token/src/token.c
+++ test_token/src/token.c
@@ -81,54 +81,123 @@ t_type get_token_type(char *word, t_token *current)
 
     return CMD;
 }
-char **split_special_chars(char *input)
+// char **split_special_chars(char *input)
+// {
+//     char **result;
+//     int i, j, k, len;
+//     char buffer[1024]; // to extract words
+
+
+//     i = 0;
+//     k = 0;
+//     len = ft_strlen(input);
+
+//     result = malloc(sizeof(char *) * (len * 2 + 1)); // making a huge malloc but idk how to do otherwise
+//     if (!result)
+//         return (NULL);
+//     while (i < len)
+//     {
+//         j = 0;
+//         while (input[i] && input[i] != ' ' && input[i] != '<' && input[i] != '>' && input[i] != '|')
+//         {
+//             buffer[j++] = input[i++];
+//         }
+//         if (j > 0) // if characters were cumulated, we stock them
+//         {
+//             buffer[j] = '\0';
+//             result[k++] = ft_strdup(buffer);
+//         }
+
+//         if (input[i] == '<' || input[i] == '>' || input[i] == '|')
+//         {
+//             if (input[i + 1] == input[i]) //  `>>` or `<<` question : if the user write a doble pipe, what should we do ? :'D 
+//             {
+//                 buffer[0] = input[i];
+//                 buffer[1] = input[i + 1];
+//                 buffer[2] = '\0';
+//                 i++;
+//             }
+//             else
+//             {
+//                 buffer[0] = input[i];
+//                 buffer[1] = '\0';
+//             }
+//             result[k++] = ft_strdup(buffer);
+//             i++;
+//         }
+//         else if (input[i] == ' ')
+//             i++;
+//     }
+
+//     result[k] = NULL;
+//     return result;
+// }
+
+//TEST SPLIT WITH VARIOUS SEPARATORS and a bit less ugly than previous one
+
+int is_separator(char c, const char *separators)
+{
+    int i = 0;
+    while (separators[i])
+    {
+        if (c == separators[i])
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int count_tokens(const char *str, const char *separators)
+{
+    int count = 0;
+    int i = 0;
+
+    while (str[i])
+    {
+        while (str[i] && is_separator(str[i], separators)) 
+            i++;
+        if (str[i])
+        {
+            count++;
+            while (str[i] && !is_separator(str[i], separators))
+                i++;
+        }
+        if (str[i] && is_separator(str[i], separators))
+        {
+            count++;
+            i++;
+        }
+    }
+    return count;
+}
+
+char **ft_split_multi(const char *str, const char *separators)
 {
     char **result;
-    int i, j, k, len;
-    char buffer[1024]; // to extract words
+    int i = 0, k = 0, word_start;
+    int num_tokens = count_tokens(str, separators);
 
-
-    i = 0;
-    k = 0;
-    len = ft_strlen(input);
-
-    result = malloc(sizeof(char *) * (len * 2 + 1)); // making a huge malloc but idk how to do otherwise
+    result = malloc(sizeof(char *) * (num_tokens + 1));
     if (!result)
         return (NULL);
-    while (i < len)
+
+    while (str[i])
     {
-        j = 0;
-        while (input[i] && input[i] != ' ' && input[i] != '<' && input[i] != '>' && input[i] != '|')
+        while(str[i] && str[i] == ' ') //we jump spaces because we don't want tokens with space
+            i++;
+        if (is_separator(str[i], separators)) 
         {
-            buffer[j++] = input[i++];
+            result[k++] = ft_substr(str, i, 1); 
+            i++;
         }
-        if (j > 0) // if characters were cumulated, we stock them
+        else
         {
-            buffer[j] = '\0';
-            result[k++] = ft_strdup(buffer);
-        }
-
-        if (input[i] == '<' || input[i] == '>' || input[i] == '|')
-        {
-            if (input[i + 1] == input[i]) //  `>>` or `<<` question : if the user write a doble pipe, what should we do ? :'D 
-            {
-                buffer[0] = input[i];
-                buffer[1] = input[i + 1];
-                buffer[2] = '\0';
+            word_start = i;
+            while (str[i] && !is_separator(str[i], separators)) 
                 i++;
-            }
-            else
-            {
-                buffer[0] = input[i];
-                buffer[1] = '\0';
-            }
-            result[k++] = ft_strdup(buffer);
-            i++;
+            result[k++] = ft_substr(str, word_start, i - word_start);
         }
-        else if (input[i] == ' ')
-            i++;
     }
-
     result[k] = NULL;
     return result;
 }
@@ -141,7 +210,7 @@ t_token *parse_input(char *input)
 {
     t_token *tokens = NULL;
     t_token *current = NULL;
-    char **words = split_special_chars(input); 
+    char **words = ft_split_multi(input, "<|> "); 
     int i = 0;
 
     if (!words)
@@ -188,14 +257,13 @@ void print_tokens(t_token *tokens)
     while (tokens)
     {
         printf("Token: [%s] | Type: [%d]\n", tokens->value, tokens->type);
-        if(tokens && tokens->previous)
-            printf("previous token value = %s previous token type = %d\n", tokens->previous->value, tokens->previous->type);
+        // if(tokens && tokens->previous)
+        //     printf("previous token value = %s previous token type = %d\n", tokens->previous->value, tokens->previous->type);
         tokens = tokens->next;
     }
 }
 
-// I tested with the input :  ls -l | grep minishell > output.txt
-// the goal was to store everything with correct type
+
 int main(void)
 {
     char *input;
