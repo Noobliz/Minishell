0260de8e795b49bc09722046bb05ef34badc7776
Lise <165786617+Noobliz@users.noreply.github.com>
2025-03-14 16:03:28 +0100

    Add files via upload

--- exec_cmds/exec_cmds.c
+++ exec_cmds/exec_cmds.c
@@ -6,16 +6,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-
-typedef struct s_all
-{
-    char            **cmds;
-    char            *cmd;
-    int             infile;
-    int             outfile;
-	//(*f)	built_in;
-    struct s_all    *next;
-} t_all;
+#include "test_token/libextract.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+// typedef struct s_all
+// {
+//     char            **cmds;
+//     char            *cmd;
+//     int             infile;
+//     int             outfile;
+// 	//(*f)	built_in;
+//     struct s_all    *next;
+// } t_all;
 
 // int	is_builtin(char *cmd)
 // {
@@ -25,36 +27,36 @@ typedef struct s_all
 // 	else
 // 		return(0);
 // }
-void    free_tab(char **tab)
+// void    free_tab(char **tab)
+// {
+//     int i;
+//     i = 0;
+//     while(tab[i])
+//     {
+//         free(tab[i]);
+//         i++;
+//     }
+//     if (tab)
+//         free(tab);
+// }
+// void    free_cmds(t_cmd *head)
+// {
+//     if (head == NULL)
+//         return ;
+//     free_cmds(head->next);
+//     if (head->argv)
+//         free_tab(head->argv);
+//     if (head->cmd)
+//         free(head->cmd);
+//     if (head->infile >= 0)
+//         close(head->infile);
+//     if (head->outfile >= 0)
+//         close(head->outfile);
+//     free(head);
+// }
+int	 count_cmds(t_cmd *cmds)
 {
-    int i;
-    i = 0;
-    while(tab[i])
-    {
-        free(tab[i]);
-        i++;
-    }
-    if (tab)
-        free(tab);
-}
-void    free_cmds(t_all *head)
-{
-    if (head == NULL)
-        return ;
-    free_cmds(head->next);
-    if (head->cmds)
-        free_tab(head->cmds);
-    if (head->cmd)
-        free(head->cmd);
-    if (head->infile >= 0)
-        close(head->infile);
-    if (head->outfile >= 0)
-        close(head->outfile);
-    free(head);
-}
-int	 count_cmds(t_all *cmds)
-{
-	t_all *tmp;
+	t_cmd *tmp;
 	tmp = cmds;
 	int count;
 
@@ -67,7 +69,7 @@ int	 count_cmds(t_all *cmds)
 	return(count);
 }
 // trying to do a bit like you :D
-void create_close_pipes(int pipes[][2], int cmd_count, t_all *cmds, int create)
+void create_close_pipes(int pipes[][2], int cmd_count, t_cmd *cmds, int create)
 {
     int i = 0;
     while (i < cmd_count - 1)
@@ -90,10 +92,10 @@ void create_close_pipes(int pipes[][2], int cmd_count, t_all *cmds, int create)
     }
 }
 
-void execute_pipeline(t_all *cmds, char **envp)
+void execute_pipeline(t_cmd *cmds, char **envp)
 {
     int     cmd_count;
-    t_all   *tmp = cmds;
+    t_cmd   *tmp = cmds;
     pid_t pid;
 	int	i;
 
@@ -167,13 +169,13 @@ void execute_pipeline(t_all *cmds, char **envp)
             //     exit(0);
             // }
             // cmd externe
-            if (tmp->cmds && (tmp->outfile != -1) && execve(tmp->cmds[0], tmp->cmds, envp) == -1)
+            if (tmp->argv && (tmp->outfile != -1) && execve(tmp->argv[0], tmp->argv, envp) == -1)
             {
                 perror(tmp->cmd);
 			    //free_cmds(tmp);
                 exit(127);
             }
-            //free_cmds(tmp); ??
+            //free_cmds(tmp);
         }
         tmp = tmp->next;
         i++;
@@ -188,7 +190,7 @@ void execute_pipeline(t_all *cmds, char **envp)
 	//free_cmds(cmds);
 }
 
-void execute_command_or_builtin(t_all *cmds, char **envp)
+void execute_command_or_builtin(t_cmd *cmds, char **envp)
 {
     if (!cmds)
         return;
@@ -198,12 +200,61 @@ void execute_command_or_builtin(t_all *cmds, char **envp)
    // else
         execute_pipeline(cmds, envp);
 }
-
+int main(int argc, char **argv, char **envp)
+{
+  (void)argc;
+  if(argv)
+  {
+	t_env	*n_env = env_item("PATH=here", 0);
+	n_env->next = env_item("NUM=herealso", 0);
+	int i = 0;
+	t_token *token = NULL;
+	//char  *str = copy("cat | <example ls -l | wc -l >output.txt");
+	t_cmd *head = NULL;
+	char *path = "/nfs/homes/lguiet/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/nfs/homes/lguiet/bin:/nfs/homes/lguiet/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin";
+  char *str;
+  while(1)
+  {
+    str = readline("minishell> ");
+	  if (!str)
+	    return (0);
+    if (*str)
+      add_history(str);
+	  token = new_token(str, CMD, NULL);
+	  if (!token)
+	    return (0);
+    i = parsing_pt_2(token, n_env);
+	  if (i == -1)
+	    printf("malloc error\n");
+	  if (i == -2)
+	    printf("only one quote\n");
+	  i = trim_split_tokens(token);
+	  if (i == -1)
+	    printf("malloc error\n");
+	//print_tokens(token);
+      spec_check(token);
+    if (assign_types(&token) == -1 || !token)
+        printf("malloc error\n");
+    cmd_shuffle(token);
+    extraction(token, &head, path);
+    print_cmds(head);
+    execute_pipeline(head, envp);
+    free(str);
+    free_cmds(head);
+  }
+  free_tokens(token);
+  free(n_env->next);
+  free(n_env);
+  
+  //print_tokens(token);
+}
+	return (0);
+}
 
 // Fonction utilitaire pour créer une commande juste pour le test
-// t_all *create_cmd(char *path, char **args, char *infile, char *outfile)
+// t_cmd *create_cmd(char *path, char **args, char *infile, char *outfile)
 // {
-//     t_all *cmd = malloc(sizeof(t_all));
+//     t_cmd *cmd = malloc(sizeof(t_cmd));
 //     if (!cmd)
 //         return NULL;
     
@@ -231,12 +282,12 @@ void execute_command_or_builtin(t_all *cmds, char **envp)
 //     (void)argc;
 //     (void)argv;
 
-//     char *args1[] = {"/bin/ls", NULL};
-//     t_all *cmd1 = create_cmd("/bin/ls", args1, NULL, "infile.txt");
+//     char *args1[] = {"/bin/lsssssssss", NULL};
+//     t_cmd *cmd1 = create_cmd("/bin/ls", args1, NULL, "infile.txt");
 //     char *args2[] = {"/usr/bin/grep", "outfile.txt", NULL};
-//     t_all *cmd2 = create_cmd("/usr/bin/grep", args2, NULL, NULL);
+//     t_cmd *cmd2 = create_cmd("/usr/bin/grep", args2, NULL, NULL);
 //     char *args3[] = {"/usr/bin/touch", "MDR", NULL};
-//     t_all *cmd3 = create_cmd("/usr/bin/touch", args3, NULL, NULL);
+//     t_cmd *cmd3 = create_cmd("/usr/bin/touch", args3, NULL, NULL);
 //     cmd1->next = cmd2;
 //     cmd2->next = cmd3;
 //     // while(cmd1)
