56df04aa8b5243b93af29cd0e107a4dc605355fc
Lise <165786617+Noobliz@users.noreply.github.com>
2025-03-20 15:53:11 +0100

    Add files via upload

--- built_ins_styx/integration_att1/exec_cmds2.c
+++ built_ins_styx/integration_att1/exec_cmds2.c
@@ -17,12 +17,12 @@ void	check_files(t_cmd *current)
 {
 	if (current->infile == -1)
 	{
-		// free_cmds
+		// free_all
 		exit(1);
 	}
 	if (current->outfile == -1)
 	{
-		// free_cmds;
+		// free_all
 		exit(1);
 	}
 }
@@ -38,37 +38,87 @@ void	update_pipe(t_cmd *current, int old_pipe[2], int new_pipe[2])
 
 void	dup_and_close(t_cmd *tmp, int old_pipe[2], int new_pipe[2], int i)
 {
-	if (tmp->infile >= 0)
+	if (tmp->infile >= 0 && tmp->built_in < 0) // there is something i don't understand here about builtins
 	{
-		dup2(tmp->infile, STDIN_FILENO);
+		if (dup2(tmp->infile, STDIN_FILENO) == -1)
+			perror("dup2");
 		close(tmp->infile);
 	}
 	else if (i > 0)
 	{
-		dup2(old_pipe[0], STDIN_FILENO);
+		if (dup2(old_pipe[0], STDIN_FILENO) == -1)
+			perror("dup2");
 		close(old_pipe[0]);
 		close(old_pipe[1]);
 	}
-	if (tmp->outfile >= 0)
+	if (tmp->outfile >= 0 && tmp->built_in < 0)
 	{
-		dup2(tmp->outfile, STDOUT_FILENO);
+		if (dup2(tmp->outfile, STDOUT_FILENO) == -1)
+			perror("dup2");
 		close(tmp->outfile);
 	}
 	else if (tmp->next)
 	{
-		dup2(new_pipe[1], STDOUT_FILENO);
+		if (dup2(new_pipe[1], STDOUT_FILENO) == -1)
+			perror("dup2");
 		close(new_pipe[0]);
 		close(new_pipe[1]);
 	}
 }
+int	exec_builtins(t_cmd *tmp, char **envp, t_env *env)
+{
+	int	infile;
+	int	outfile;
+	if (tmp->infile >= 0)
+	{
+		printf("infile");
+		infile = dup(0); //sauvegarde de stdin
+		if (infile == -1)
+			return (-1);
+		close(0);
+		if (dup2(tmp->infile, STDIN_FILENO) == -1)
+			return (-1);
+		close(tmp->infile);
+		//tmp->infile = -2; // need to discuss this
+	}
+	if (tmp->outfile >= 0)
+	{
+		printf("outfile");
+		outfile = dup(1);
+		if (outfile == -1)
+			return (-1);
+		close(1);
+		if (dup2(tmp->outfile, STDOUT_FILENO) == -1)
+			return (-1);
+		close(tmp->outfile);
+	}
+	if (built_in_att1(tmp->built_in, tmp->argv, envp, env) == -1)
+		return (-1);
+	if (tmp->infile >= 0)
+	{
+		close(0);
+		if (dup2(infile, 0) == -1) // restauration de stdin
+			perror("dup2");
+		close(infile);
+	}
+	if (tmp->outfile >= 0)
+	{
+		close(1);
+		if (dup2(outfile, 1) == -1)
+			perror("dup2") ;
+		close(outfile);
+	}
+	return (0);
+}
+
 void	execute_pipeline(t_cmd *cmds, t_env *env, char **envp)
 {
 	t_cmd	*tmp;
 	int		i;
-
-	tmp = cmds;
 	int old_pipe[2]; // Stocke le pipe précédent
 	int new_pipe[2]; // Stocke le pipe actuel
+
+	tmp = cmds;
 	i = 0;
 	while (tmp)
 	{
@@ -93,21 +143,21 @@ void	execute_pipeline(t_cmd *cmds, t_env *env, char **envp)
 			dup_and_close(tmp, old_pipe, new_pipe, i);
 			if (tmp->built_in >= 0)
 			{
-				if(built_in_att1(tmp->built_in, tmp->argv, envp, env))
-				{
-					//free_all
-					exit(0);
-				}
-				else
+				if (exec_builtins(tmp, envp, env) == -1)
 				{
 					//free_all
 					exit(1);
 				}
+				else
+				{
+					//free_all
+					exit(0);
+				}
 			}
-			if (tmp->argv && execve(tmp->argv[0], tmp->argv, envp) == -1)
+			else if (tmp->argv && execve(tmp->argv[0], tmp->argv, envp) == -1)
 			{
 				perror(tmp->cmd);
-				// free_cmds
+				// free_all
 				exit(127);
 			}
 		}
@@ -132,36 +182,46 @@ void	execute_command_or_builtin(t_cmd *cmds, t_env *env, char **envp)
 {
 	t_cmd	*tmp;
 	int		status;
-
 	if (!cmds)
 		return ;
 	tmp = cmds;
-	// only one cmd and not a builtin
-	if (cmds->next == NULL && cmds->built_in < 0)
+	// only one cmd
+	if (cmds->next == NULL)
 	{
+		if (cmds->built_in >= 0)
+		{
+			exec_builtins(tmp, envp, env);
+			return ;
+		}
 		tmp->pid = fork();
 		if (tmp->pid == 0)
 		{
 			if (tmp->infile >= 0)
 			{
-				dup2(tmp->infile, STDIN_FILENO);
+				if(dup2(tmp->infile, STDIN_FILENO) == -1)
+				{
+					perror("dup2");
+					//free_all
+				}
 			}
 			if (tmp->outfile >= 0)
 			{
-				dup2(tmp->outfile, STDOUT_FILENO);
+				if(dup2(tmp->outfile, STDOUT_FILENO) == -1)
+				{
+					perror("dup2");
+					//free_all
+				}
 			}
 			if (tmp->argv && execve(tmp->argv[0], tmp->argv, envp) == -1)
 			{
 				perror(tmp->cmd);
+				//free_all
 				exit(127);
 			}
 		}
 		waitpid(tmp->pid, &status, 0);
 		return ;
 	}
-	// if builtin alone
-	else if (cmds->next == NULL && cmds->built_in >= 0)
-		built_in_att1(cmds->built_in, cmds->argv, envp, env);
 	// if pipeline
 	else
 		execute_pipeline(cmds, env, envp);
