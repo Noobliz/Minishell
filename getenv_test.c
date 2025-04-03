2cf5e71bada4e34945eb5660f19463966c9dd629
styx-trav <naomi.rakotomanga@gmail.com>
2025-04-03 15:55:20 +0200

    delete all

--- getenv_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(void)
-{
-	printf("here the path var :: %s\n", getenv("PATH"));
-	printf("here the var var :: %s\n", getenv("VAR"));
-	return (0);
-}
